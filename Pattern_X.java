58e5eac5b29646083689a0958dc045be679bf671
Raufun Nasim Khan Chowdhury <44887399+Shishir47@users.noreply.github.com>
2022-08-27 01:32:33 +0600

    Update Pattern_X.java

--- Pattern_X.java
+++ Pattern_X.java
@@ -5,20 +5,19 @@ public class  Pattern_X{
 	Scanner sc=new Scanner(System.in);
 	System.out.print("Enter N : ");
 	int n=sc.nextInt();	 
-    int k=n*2-1;
-    System.out.println();
-    for(int i=1;i<=k;i++){
-                    
-	        for(int j=1;j<=k;j++){      
+   	int k=n*2-1;
+    	System.out.println();
+    	for(int i=1;i<=k;i++){
+        	for(int j=1;j<=k;j++){      
 	        	if(j==i || j==k-i+1)
-                System.out.print(i);
-                System.out.print(" ");
+                	System.out.print(i);
+                	System.out.print(" ");
 	        }
 		 
     
-	 System.out.println();
+	 	System.out.println();
 	    
-               }            
+  	}            
                 
     }
 }
